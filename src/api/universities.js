70b8e6b5cfd28355960aee56b538d63eb09bab2b
Alexandra <alexandra@MacBook-Pro-de-Alexandra.local>
2025-07-26 18:50:57 -0500

    feat: update env var

--- src/api/universities.js
+++ src/api/universities.js
@@ -1,11 +1,13 @@
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 export async function fetchUniversities() {
-  const response = await fetch('https://uniguia-api.onrender.com/uniguia/api/v1/universidades');
+  const response = await fetch(`${BASE_URL}/uniguia/api/v1/universidades`);
   if (!response.ok) throw new Error('Error al obtener universidades');
   return response.json();
 }
 
 export async function addUniversity(data) {
-  const response = await fetch('https://uniguia-api.onrender.com/uniguia/api/v1/universidades', {
+  const response = await fetch(`${BASE_URL}/uniguia/api/v1/universidades`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
